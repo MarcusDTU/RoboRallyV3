571f4990ef2e214fe3e23b9c68ef8e2b7c9d8350
Marcus Langkilde <s195080@dtu.dk>
2024-05-03 05:42:46 +0200

    added javadoc to the classes and methods

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/DiscardPile.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/DiscardPile.java
@@ -2,6 +2,11 @@ package dk.dtu.compute.se.pisd.roborally.model;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a discard pile of command cards.
+ * @author Marcus Langkilde, s195080@DTU.dk
+ * @author Haleef Abu Talib, s224523@dtu.dk
+ */
 public class DiscardPile {
 
     public ArrayList<Command> pile = new ArrayList<>();
